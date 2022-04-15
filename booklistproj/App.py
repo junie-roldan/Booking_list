690ca2f69efc49f7e2dd58db9c1bbd21c92bb783
NiceGuy300 <johnjunie35@gmail.com>
2022-04-15 16:45:28 +0800

    edited App

--- booklistproj/App.py
+++ booklistproj/App.py
@@ -4,7 +4,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.secret_key = "Secret"
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:chadthepro123@localhost/booklist'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    rate = db.Column(db.String(100))
+    rtype = db.Column(db.String(100))
+    arrival = db.Column(db.String(100))
+    departure = db.Column(db.String(100))
+
+
+def __init__(self, name, email, rate, rtype, arrival, departure):
+ 
+        self.name = name
+        self.email = email
+        self.rate = rate
+        self.rtype = rtype
+        self.arrival = arrival
+        self.departure = departure
 
 @app.route('/')
 def Index():
